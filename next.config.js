946e07c11603a5bd3f8032f6d58a8eccf088b088
MeetRohan <raudramittal@gmail.com>
2025-04-03 14:24:04 +0530

    changes nextjsconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD:next.config.js
-=======
- 
->>>>>>> 2cdd5f8 (Initial commit):next.config.mjs
   async redirects() {
     return [
       {
